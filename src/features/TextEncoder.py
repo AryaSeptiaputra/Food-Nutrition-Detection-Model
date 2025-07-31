1b6c46ec212e9838a8e7abbeeeec52eeb9b65f8b
AryaSeptiaputra <aryaekaseptiaputra016@gmail.com>
2025-07-31 20:11:14 +0700

    Updating preprocessing data

--- src/features/TextEncoder.py
+++
@@ -1,8 +0,0 @@
-from langchain_community.embeddings import SentenceTransformerEmbeddings
-
-class TextEncoder:
-    def __init__(self, model_name="all-MiniLM-L6-v2"):
-        self.encoder = SentenceTransformerEmbeddings(model_name=model_name)
-
-    def get_encoder(self):
-        return self.encoder
